5511d8c582e61dc654aa2fa6280cb0c296b30c5c
Subhashish Ghosh <subhashishghosh2025@gmail.com>
2025-12-27 14:37:17 +0530

    Fixed bug related to several beans of same types which failed the app to start

--- src/main/java/com/practice/spboot/manageemp/controller/ManageEmployeeController.java
+++ src/main/java/com/practice/spboot/manageemp/controller/ManageEmployeeController.java
@@ -1,5 +1,8 @@
 package com.practice.spboot.manageemp.controller;
 
+import com.practice.spboot.manageemp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ManageEmployeeController {
 
+    @Autowired
+    @Qualifier("employeeMockServiceImpl")
+    private EmployeeService employeeService;
+
     @RequestMapping(path = "/testemp", method = RequestMethod.GET)
     public String testController() {
         return "ManageEmployeeController is working fine";
     }
+
 }
